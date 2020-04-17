d2cc74120957352867d5ccfea7239f0193fe0c2e
erickpoleto <erick-poleto@hotmail.com>
2020-04-17 17:37:11 -0300

    all set, email, frontend, map

--- frontend/src/pages/footer/index.js
+++ frontend/src/pages/footer/index.js
@@ -6,7 +6,6 @@ import './styles.css'
 export default function Footer() {
     return(
         <footer>
-            <strong>BeerS social medias</strong>
             <p>Use of this website is subject to the terms and conditions 
                 of the Terms of Use and Privacy Policy.</p>
             <b>© BeerS. Todos os direitos reservados.</b>
